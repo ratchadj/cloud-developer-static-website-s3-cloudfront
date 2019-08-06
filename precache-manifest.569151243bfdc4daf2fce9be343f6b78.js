1f400ee00f7da92a8ff04f77a6f1ce1cc5ac96da
ratchada.jududom <ratchada.jududom@gmail.com>
2019-08-06 23:25:42 +0700

    init

---
+++ precache-manifest.569151243bfdc4daf2fce9be343f6b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373e38707f5b6f552851e4e157d70e33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "907ee30e2ae10fcbf55c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "907ee30e2ae10fcbf55c",
+    "url": "/static/js/main.6f82e569.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
